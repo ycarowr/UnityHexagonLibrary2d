a82004aacb2ac1d539c7730847bea4c4bce0b021
ycarowr <ywrycaro@gmail.com>
2019-12-27 17:36:18 +0900

    Add unit data to make more clear where to put units in the board

--- Assets/Scripts/BoardSystem/Board/Position.cs
+++ Assets/Scripts/BoardSystem/Board/Position.cs
@@ -3,12 +3,32 @@
     /// <summary>
     ///     A position in a real game most likely stores some sort of data.
     ///     Things like heroes, buildings, monsters, itens, etc. Be creative.
+    ///     <remarks> If this structure grow consider make it a class instead a struct. </remarks>>
     /// </summary>
     public struct Position
     {
+        /// <summary>
+        ///     The unit(s) in the board.
+        ///     <remarks> Consider make an Array if it can hold more than one unit. </remarks>>
+        /// </summary>
+        public BoardUnit Unit { get; private set; } 
         public Hex Point { get; }
-        public Position(Hex point) => Point = point;
+        public Position(Hex point, BoardUnit unit = null)
+        {
+            Point = point;
+            Unit = unit;
+        }
 
-        //include the data here
+        public void AddUnit(BoardUnit unit) => Unit = unit;
+        public void RemoveUnit(BoardUnit unit) => Unit = null;
+        public bool HasUnit() => Unit != null;
+    }
+
+    /// <summary>
+    ///     Any kind of unit which can be positioned inside the board.
+    /// </summary>
+    public abstract class BoardUnit
+    {
+        
     }
 }
