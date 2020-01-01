bc5ceda5e2b60c4a27c246a63bf8ea7310f1dfe9
ycarowr <ywrycaro@gmail.com>
2020-01-01 15:02:31 +0900

    Add tutorial phrase

---
+++ Assets/Scripts/Ui/Util/UiTutorial.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UiTutorial : MonoBehaviour
+{
+    [SerializeField] Button close;
+    [SerializeField] GameObject content;
+    void Awake() => close.onClick.AddListener(Close);
+    void Close() => content.SetActive(false);
+}
