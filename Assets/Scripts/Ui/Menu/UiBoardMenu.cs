09a3789a4a5012c8db207fc16aa5f2c6432ed9a2
ycarowr <ywrycaro@gmail.com>
2020-01-01 17:37:11 +0900

    Add error dispatch for uncompatiable screen sizes (demands a bug fix)

--- Assets/Scripts/Ui/Menu/UiBoardMenu.cs
+++ Assets/Scripts/Ui/Menu/UiBoardMenu.cs
@@ -5,7 +5,11 @@ namespace HexCardGame.UI
 {
     public class UiBoardMenu : UiParentMenu
     {
+        const float Width = 1920;
+        const float Height = 1080;
         const int ABigLength = 12;
+        [SerializeField] [Tooltip("Change the resolution to make the placement of the menu (it depends on the aspect ratio).")]
+        bool isSetResolution = true;
         [SerializeField] UiBoardHightlight boardHighlight;
         [SerializeField] Button diagonalAscButton;
         [SerializeField] Button diagonalDesButton;
@@ -28,6 +32,8 @@ namespace HexCardGame.UI
             var rectSize = menu.rect.size;
             var offsetX = rectSize.x / 2;
             var offsetY = -rectSize.y / 2;
+            //TODO (Bug):
+            //Demands Full HD resolution or any equivalent aspect ratio (1.77), otherwise the placement is done in a wrong way.
             menu.anchoredPosition = (screenPoint/factorResolution + new Vector2(offsetX, offsetY));
             Selection = selection;
             Show();
@@ -36,6 +42,10 @@ namespace HexCardGame.UI
         protected override void Start()
         {
             base.Start();
+            var delta = Mathf.Abs(Camera.main.aspect - Width / Height) > 0.001f;
+            if(delta)
+                Debug.LogError("Demands Full HD resolution or any equivalent aspect ratio (1.77). " +
+                               "Otherwise the 'Right Click Menu' placement is done in a wrong way.");
             neighboursButton.onClick.AddListener(OnPressNeighbours);
             diagonalDesButton.onClick.AddListener(OnPressDiagonalDes);
             diagonalAscButton.onClick.AddListener(OnPressDiagonalAsc);
